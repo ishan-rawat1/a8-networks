41dfe5871ec9ac8de034686a6d86f342561a1c32
ishan-rawat1 <ishanrawat111@gmail.com>
2024-10-22 19:08:23 -0400

    added gethostbyname function and done assignment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 
 # Try this with example.com first, but try changing it to a different site after
 # you get it working.
-hostname = "example.com"
+hostname = "worldtimeapi.org"
 
 
 # Do not modify this function
@@ -34,7 +34,9 @@ def make_http_request(ip, port):
 # (This variable is defined at the top of this file if you want to play with it)
 #
 # Hint: This is meant for you to use Google to figure out.
-
+ip_address = socket.gethostbyname(hostname)
+response = make_http_request(ip_address, 80)
+print(response)
 # Next, call make_http_request with the correct parameters and then print the response
 # This response should look a lot like the in-class example of "curl", and start with
 # "HTTP/1.1 200 OK". Rememeber, we're making an HTTP request, so from that you should
